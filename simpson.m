65a91ef2f8b98294d945931e773e91f8ff2fe0fc
AyseYuksel <68689527+AyseYuksel@users.noreply.github.com>
2021-05-04 06:25:25 -0700

    initial commit

---
+++ simpson.m
@@ -0,0 +1,28 @@
+﻿%for example
+%simpson('e^x',0,1)
+%ans = 1.7189
+
+%the actual value of the integral of the function
+%octave:16> syms x
+%octave:17> int(e^x,0,1)
+%ans = (sym) -1 + ℯ
+%octave:18> eval(ans)
+%ans = 1.7183
+
+
+%actual value -> 1.7183
+%approximate value -> 1.7189
+
+
+
+function y=simpson(f,a,b)
+
+str1='@(x)';
+str2=f;
+str=[str1 str2];
+ff=str2func(str);
+
+x1=(a+b)/2;
+h=x1;
+
+y=(h/3).*(ff(a)+4.*ff(x1)+ff(b));
---
+++ trapezoidal.m
@@ -0,0 +1,22 @@
+﻿%for example 
+%trapezoidal('x.^2',0,1)
+%ans = 0.5000
+
+%the actual value of the integral of the function
+%octave:12> syms x
+%octave:13> int(x^2,0,1)
+%ans = (sym) 1/3
+%octave:14> eval(ans)
+%ans = 0.3333
+
+%actual value -> 0.3333
+%approximate value ->0.5000
+
+function y=trapezoidal(f,a,b)
+
+str1='@(x)';
+str2=f;
+str=[str1 str2];
+ff=str2func(str);
+
+y=(ff(a)+ff(b))/2;
